8546b0a6a2824ccfb38ac57c08ccfca229bc2d08
paulakelo <akelopauljudeochieng@gmail.com>
2024-11-18 17:16:28 +0300

    Add index views and update URL patterns for accounts and expenses apps

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')
-    else:
-        form = UserCreationForm()
-    return render(request, 'registration/register.html', {'form': form})
+
+def index(request):
+    return HttpResponse("Hello, You're at the accounts index.")
--- urls.py
+++ urls.py
@@ -4,4 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),  # Django admin panel URL
     path('tracker/', include('tracker.urls')),  # Include URLs from the tracker app
+    path('expenses/', include('expenses.urls')), # Include URLs from the expenses app
+    path('accounts/', include('accounts.urls')) # Include URLs from the accounts app
 ]
