30c0f892a2c3d7f3d560f39a2badd53b93202451
paulakelo <akelopauljudeochieng@gmail.com>
2024-11-18 16:51:43 +0300

    Fix URL pattern for tracker app by adding missing slash

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # Django admin panel URL
-    path'', include('tracker.urls')),  # Include URLs from the tracker app
+    path('tracker/', include('tracker.urls')),  # Include URLs from the tracker app
 ]
