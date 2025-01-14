c9c05b2b9b96b56b4d9c6819cf5a81d1268f76eb
Paul Ochieng <akelopauljudeochieng@gmail.com>
2025-01-14 17:28:37 +0300

    Remove unused files and code from personal financial tracker project

--- tracker/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('expenses/', views.expense_list, name='expense_list'),  # URL for viewing expenses
-    path('income/', views.income_list, name='income_list'),  # URL for viewing income
-    path('', views.home, name='home'),
-]
-
--- urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),  # Django admin panel URL
-    path'', include('tracker.urls')),  # Include URLs from the tracker app
-]
